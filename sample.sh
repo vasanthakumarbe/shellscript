d7a7093e4bf315e11d93dc4222c8655c53a1b25c
vasanthakumarbe <110677150+vasanthakumarbe@users.noreply.github.com>
2022-09-21 10:18:58 -0600

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,5 +1,5 @@
-ls 
-pwd
-mkdir vasanth
-ls
-ls -lrt
+#!/bin/bash
+monthsback=1
+monthsfwd=`expr $monthsback - 1`
+startdate=`date -d "-${monthsback} month -$(($(date +%d)-1)) days" +%Y-%m-%d`
+enddate=`date -d "-$(date +%d) days -${monthsfwd} month" +%Y-%m-%d
